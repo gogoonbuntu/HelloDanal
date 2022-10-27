22ea93f7a8dd2e0606d680fa6d4cb298d47132ba
gogoonbuntu <tmddud333@naver.com>
2022-10-27 01:39:31 +0000

    update

--- dbconn.php
+++ dbconn.php
@@ -1,12 +1,13 @@
 <?php
 
-    $host = "127.0.0.1";
-    $id = "root";
+    $host = "db";
+    $port = "6033";
+    $id = "tmddud333";
     $pw = "abd123"; //보안보안
     $dbname = "tmddud333";
-	$conn = mysqli_connect($host, $id, $pw, $dbname);
+	$conn = mysqli_connect($host, $id, $pw, $dbname, $port);
 
-  $db = new mysqli($host, $id, $pw, $dbname);
+  $db = new mysqli($host, $id, $pw, $dbname, $port);
 	//$db->set_charset("utf8");
 
 	function mq($sql)
