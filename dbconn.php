e1ed9454aa89e68e0ca03093d7a8354191f6506d
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2023-02-21 23:24:57 +0900

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,21 @@
 <?php
 
+    $db_docker = array(
+	    'host'=>'db',
+	    'port'=>"3306",
+	    'id'=>'tmddud333',
+	    'pw'=>'abd123',
+	    'dbname'=>'tmddud333'
+    );
+
+    $db_db4free = array(
+	    'host'=>'db4free.net',
+	    'port'=>"3306",
+	    'id'=>'tmddud333',
+	    'pw'=>'', //in email
+	    'dbname'=>'tmddud333'
+    );
+
     $host = "db";
     $port = "3306";
     $id = "tmddud333";
